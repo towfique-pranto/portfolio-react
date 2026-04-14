65cf19af4b666062de1648ab6a78beb31f6fa62b
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-14 21:46:11 +0600

    feat: implement main structure with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import HeaderLeft from "./components/HeaderLeft";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Certifications from "./components/Certifications";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div>APP</div>;
+  return (
+    <div className="flex min-h-screen w-full">
+      <HeaderLeft />
+
+      <main className="flex-3">
+        <Home />
+        <Projects />
+        <Certifications />
+        <Contact />
+      </main>
+    </div>
+  );
 }
 
 export default App;
