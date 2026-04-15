4eecd71dcbabc8257b8c15d9ae34a6d152989280
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-15 16:30:31 +0600

    feat: update Home component image and enhance text styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import image from "../assets/myImagebgRemoved.png";
+import image from "../assets/image.jpg";
 
 const Home = () => {
   return (
@@ -9,9 +9,13 @@ const Home = () => {
       <div className="flex flex-col items-center justify-center gap-8 p-5 text-center">
         <img src={image} alt="My Image" className="w-55 md:w-65 rounded-full" />
         <div className="space-y-1 md:space-y-3">
-          <h1>Towfique Kabir</h1>
-          <h3>Web Developer</h3>
-          <p>
+          <h1 className="bg-linear-to-r from-teal-400 to-teal-600 bg-clip-text text-transparent text-3xl font-semibold md:text-4xl lg:text-5xl">
+            Towfique Kabir
+          </h1>
+          <h3 className="bg-linear-to-r from-teal-400 to-teal-600 bg-clip-text text-transparent text-xl font-semibold md:text-2xl lg:text-3xl">
+            Web Developer
+          </h3>
+          <p className="max-w-125 text-sm text-gray-900 md:text-base lg:text-lg">
             I'm a web developer working with React and modern JavaScript. I
             build clean, structured user interfaces and turn ideas into
             practical, real-world applications, with a focus on scalability and
