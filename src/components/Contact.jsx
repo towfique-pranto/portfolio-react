1c4ad84970b2185ece3b1ee687dede6b1cef2e20
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-17 21:48:37 +0600

    feat: enhance Contact component layout with form and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      id="contact"
+      className="flex min-h-screen w-full flex-col items-center justify-center gap-16 p-8"
+    >
+      <h1 className="text-center text-6xl font-light text-teal-600">
+        Contact me
+      </h1>
+
+      <form className="flex w-full max-w-md flex-col gap-8 rounded-lg p-6 md:max-w-lg lg:max-w-xl">
+        <div className="flex flex-col gap-4">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Your firstname"
+            className="rounded-lg border-2 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-teal-50 focus:ring-2 focus:ring-teal-500"
+          />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Your lastname"
+            className="rounded-lg border-2 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-teal-50 focus:ring-2 focus:ring-teal-500"
+          />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Your email"
+            className="rounded-lg border-2 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-teal-50 focus:ring-2 focus:ring-teal-500"
+          />
+        </div>
+        <textarea
+          name=""
+          id=""
+          placeholder="Write your message here..."
+          className="h-32 w-full resize-none rounded-lg border-2 px-4 py-3 text-lg outline-none transition-all duration-200 hover:bg-teal-50 focus:ring-2 focus:ring-teal-500"
+        ></textarea>
+        <button className="rounded-lg border-2 px-6 py-3 border-teal-400 bg-teal-500 text-white hover:bg-teal-600 transition-all duration-200">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
