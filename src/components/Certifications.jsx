a83224a51b0701cd334b191046dc2853d229c76e
Towfique Kabir <towfique.pranto@gmail.com>
2026-04-17 21:09:13 +0600

    feat: enhance Certifications component layout and update HeaderLeft styles

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Certifications = () => {
   return (
-    <div id="certifications" className="">
-      Certifications
+    <div
+      id="certifications"
+      className="flex min-h-screen w-full flex-col items-center justify-center gap-20 p-14 md:p-20 lg:p-36"
+    >
+      <h1 className="text-center text-5xl font-light">Certifications</h1>
+      <div></div>
     </div>
   );
 };
--- src/components/HeaderLeft.jsx
+++ src/components/HeaderLeft.jsx
@@ -6,13 +6,13 @@ import { MdContactMail } from "react-icons/md";
 
 const HeaderLeft = () => {
   return (
-    <div className="md:flex-1">
+    <div className="md:flex-1 max-w-75">
       <div className="min-h-screen top-0 hidden md:sticky md:flex items-center bg-teal-600 text-white">
         <ul className="flex flex-col gap-10">
           {["home", "projects", "certifications", "contact"].map((item) => (
             <li
               key={item}
-              className="flex items-center justify-start cursor-pointer font-medium transition-all duration-200 group sm:text-lg mdLtext-xl xl:text-3xl"
+              className="flex items-center justify-start cursor-pointer font-medium transition-all duration-200 group sm:text-lg mdLtext-xl"
             >
               <IoMdArrowDropright className="text-4xl -translate-x-5 opacity-0 transform transition-all duration-200 group-hover:opacity-100 group-hover:translate-x-0" />
               <a
